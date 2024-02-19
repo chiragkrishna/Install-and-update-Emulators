f8a9c82a174821edd355becbaf0cc0b84d140bef
chiragkrishna <chiragkrishna@gmail.com>
2024-02-20 00:00:55 +0530

    correction

--- update.ps1
+++ update.ps1
@@ -216,7 +216,7 @@ else {
     Remove-Item downloads/ppsspp*windows-amd64.7z -Recurse -Force
     Invoke-WebRequest -Uri "$downloadUrl" -OutFile downloads/"$filename" -Headers @{
         "Referer"    = "https://buildbot.orphis.net";
-        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
     }
     7z x downloads/$filename -o"temp" -y
     if ($LASTEXITCODE -eq 0) {
